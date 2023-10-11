a278e1defc4a27006689526cdf3044bd7de1afaa
PatrickTschaikner <patrick.tschaikner@student.htldornbirn.at>
2023-10-11 09:03:18 +0200

    Update

--- 2023/DiceGame/src/Main.java
+++ 2023/DiceGame/src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,6 +10,9 @@ public class Main {
         Random random = new Random();
         String strStart = "0";
         int SummeC = 0;
+        int SummeS = 0;
+        int PunkteC = 0;
+        int PunkteS = 0;
 
         System.out.println("--------------------Willkommen zum DICE GAME!--------------------");
         System.out.println("Schreiben Sie 'Start' um Das Spiel anzufangen");
@@ -17,8 +21,22 @@ public class Main {
             int Computer = random.nextInt(6) + 1;
             int Player = random.nextInt(6) + 1;
 
-            SummeC = SummeC + Computer;
+            if(Computer > Player){
+                PunkteC++;
+            }else if(Player > Computer){
+                PunkteS++;
+            }
+            //SummeC = SummeC + Computer;
+            //SummeS = SummeS + Player;
             System.out.println("Computer: " + Computer + "\t Spieler: " + Player);
+        }
+        System.out.println("C: " + PunkteC + " S: " + PunkteS);
+        if(PunkteC > PunkteS){
+            System.out.println("Der Computer hat gewonnen!");
+        }else if(PunkteS > PunkteC){
+            System.out.println("Der Spieler hat gewonnen!");
+        }else{
+            System.out.println("Gleichstand!");
         }
     }
 }
